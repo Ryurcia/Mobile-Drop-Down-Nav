3ce06ec057b22cfedcae499589fb68bc4a756724
Ryle <itsryle.urcia@gmail.com>
2020-10-21 00:07:29 -0400

    Added some simple documentation

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ const nav = document.querySelector('.nav');
 const openMenu = document.querySelector('.open');
 const navItem = document.querySelector('.nav-items');
 
+// Listen for user click
 openMenu.addEventListener('click', () => {
     nav.classList.toggle('mobileMenu');
 });
 
+// Listen for when Item gets clicked/pressed
 navItem.addEventListener('click', (e) => {
     if(e.target.classList.contains('nav-link')) {
         nav.classList.toggle('mobileMenu');
